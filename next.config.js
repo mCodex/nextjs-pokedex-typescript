ef396bc7172a9e448f756f7245b509e79ced5802
Mateus Andrade <mat.andrade@live.com>
2020-10-28 09:40:50 -0300

    Updating to nextjs 10

--- next.config.js
+++ next.config.js
@@ -2,4 +2,5 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   assetPrefix: isProd ? 'https://mcodex.dev/nextjs-pokedex-typescript' : '',
+  hostname: 'https://pokeres.bastionbot.org/images/pokemon/',
 };
