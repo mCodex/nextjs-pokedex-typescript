a6c7d1e68807beee61fe07a86fbd4aca336b2b20
Mateus Andrade <mat.andrade@live.com>
2020-10-28 09:41:56 -0300

    Removed unused hostname in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,4 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   assetPrefix: isProd ? 'https://mcodex.dev/nextjs-pokedex-typescript' : '',
-  hostname: 'https://pokeres.bastionbot.org/images/pokemon/',
 };
