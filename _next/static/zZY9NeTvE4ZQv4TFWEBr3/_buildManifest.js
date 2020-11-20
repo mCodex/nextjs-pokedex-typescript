5028818c67d47f1459def79b4232ea86abace242
mCodex <mCodex@users.noreply.github.com>
2020-11-20 12:30:20 +0000

    deploy: 4398bf54f9d90c34fc24980d17dd4f8ed0aa2622

---
+++ _next/static/zZY9NeTvE4ZQv4TFWEBr3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-b55fc3f237a273a950c5.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-0b6d38c65c64561a25ab.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
